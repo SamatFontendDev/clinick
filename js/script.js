17d6ea45d445da28e0f0def2e6a2e8311f0d793b
Хабибуллин Самат <D:\mail>
2020-01-24 11:55:30 +0300

    24.01.19

--- js/script.js
+++ js/script.js
@@ -115,5 +115,20 @@ $(document).ready(function(){
    });
 
     //gallery
-    $('[data-fancybox="gallery"]').fancybox({});    
+    $('[data-fancybox="gallery"]').fancybox({});
+
+    // froms
+    // $("#form").submit(function(e) { //устанавливаем событие отправки для формы с id=form
+    //     e.preventDefault();
+    //     var form_data = $(this).serialize(); //собераем все данные из формы
+    //     $.ajax({
+    //     type: 'POST', //Метод отправки
+    //     url: 'send.php', //путь до php фаила отправителя
+    //     data: form_data,
+    //             success: function(data){ // сoбытиe пoслe удaчнoгo oбрaщeния к сeрвeру и пoлучeния oтвeтa
+    //             alert('все ок'); // пoкaжeм eё тeкст
+    //             }
+    //     });
+    // });
 });
+ 
